80baa6c043a24fcd1b157ec4569a1f75ff28e3ee
Jerick Iquin <jci.dev.0@gmail.com>
2023-03-15 19:05:25 -0700

    added page piling

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,8 @@
 import '../styles/styles.scss';
 import { config } from './game/index.js';
-import { sectionScroller } from './utils/helpers';
+import $ from 'jquery';
+
+window.$ = $;
 
 const initGame = () => {
   const game = new Phaser.Game(config);
@@ -8,8 +10,38 @@ const initGame = () => {
 
 // ------------------------ Event listeners ------------------------
 
+$(document).ready(function () {
+  $('#pagepiling').pagepiling({
+    menu: '#menu',
+    direction: 'vertical',
+    verticalCentered: true,
+    sectionsColor: [],
+    anchors: ['about', 'skills', 'projects'],
+    scrollingSpeed: 700,
+    easing: 'swing',
+    loopBottom: false,
+    loopTop: false,
+    css3: true,
+    navigation: {
+      textColor: '#000',
+      bulletsColor: '#000',
+      position: 'bottom',
+      tooltips: ['about', 'skills', 'projects'],
+    },
+    normalScrollElements: null,
+    normalScrollElementTouchThreshold: 5,
+    touchSensitivity: 5,
+    keyboardScrolling: true,
+    sectionSelector: '.section',
+    animateAnchor: false,
+
+    //events
+    // onLeave: function (index, nextIndex, direction) {},
+    // afterLoad: function (anchorLink, index) {},
+    // afterRender: function () {},
+  });
+});
+
 document.addEventListener('DOMContentLoaded', () => {
   initGame();
 });
-
-document.body.addEventListener('scroll', sectionScroller);
--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,15 @@
 import { defineConfig } from 'vite';
+import inject from '@rollup/plugin-inject';
 
 const path = require('path');
 
 export default defineConfig({
+  plugins: [
+    inject({
+      $: 'jquery',
+      jQuery: 'jquery',
+    }),
+  ],
   resolve: {
     alias: {
       '@/*': path.resolve(__dirname, './*'),

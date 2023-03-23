107acf49db1280de794d831e0846e93588eeac51
Jerick Iquin <jci.dev.0@gmail.com>
2023-03-23 08:17:10 -0700

    fixed background for noscript and added placeholder for full absolute

--- scripts/page/parallax.js
+++ scripts/page/parallax.js
@@ -6,10 +6,11 @@ export const initProjectParallax = () => {
     const x = e.clientX;
     const y = e.clientY;
     const midX = window.innerWidth / 2;
-    const midY = window.innerHeight;
-    const factorX = (x - midX) / 3000;
-    const factorY = (y - midY) / 3000;
+    const height = window.innerHeight;
+    // factors so that movement are as minimal as possible but still visible
+    const factorX = (x - midX) / (midX * 5);
+    const factorY = (y - height) / (height * 2.5);
 
-    foreground.style.transform = `translateX(${-factorX}%)  translateY(${-factorY}%) translateZ(1rem)`;
+    foreground.style.transform = `translateX(${-factorX}%)  translateY(${-factorY}%) scaleX(1.01)`;
   });
 };
